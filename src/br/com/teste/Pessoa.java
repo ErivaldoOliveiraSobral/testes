ba6989ea1900e1683545d5fab60ad84b80b77d02
ErivaldoOliveiraSobral <erivaldooliveirasobral@gmail.com>
2017-03-09 20:33:18 -0300

    Retirada do cpf

--- src/br/com/teste/Pessoa.java
+++ src/br/com/teste/Pessoa.java
@@ -4,7 +4,6 @@ public abstract class Pessoa {
 	
 	private String nome;
 	private int telefone;
-	private int cpf;
 	
 	public String getNome() {
 		return nome;
@@ -18,13 +17,4 @@ public abstract class Pessoa {
 	public void setTelefone(int telefone) {
 		this.telefone = telefone;
 	}
-	public int getCpf() {
-		return cpf;
-	}
-	public void setCpf(int cpf) {
-		this.cpf = cpf;
-	}
-	
-	
-
 }
