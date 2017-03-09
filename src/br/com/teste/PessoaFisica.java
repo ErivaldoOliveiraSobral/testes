345216660b6f9e50f6b10f16e6d19998afac0743
ErivaldoOliveiraSobral <Erivaldo@Erivaldo-PC>
2017-03-09 20:37:12 -0300

    Quarta Mudança

--- src/br/com/teste/PessoaFisica.java
+++ src/br/com/teste/PessoaFisica.java
@@ -2,5 +2,14 @@ package br.com.teste;
 
 public class PessoaFisica extends Pessoa {
 	
+	private int cpf;
+
+	public void setCpf(int cnpj) {
+		this.cpf = cnpj;
+	}
+
+	public int getCpf() {
+		return cpf;
+	}
 	
 }
